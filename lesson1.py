84d1c4accf08395ef15aaf8c6437e260006df397
Elchin Bayramli <112571605+elcinbayramoff@users.noreply.github.com>
2024-08-29 14:59:05 +0400

    Alqoritmlər və Python-a giriş

---
+++ lesson1.py
@@ -0,0 +1,70 @@
+#Şərhlər/ comment dsfs
+
+# Bunlar icra olunmur!!
+
+"""sdfsd
+Bunlar
+Serhdir
+ds
+dsf
+"""
+
+'''
+Bunlar 
+da serhlerdir
+sdasd
+'''
+
+#Print funksiyasi
+
+# print('Hello world!')
+
+# print()
+# print()
+# print()
+
+# ctrl + .
+
+
+# print('Süd nə rəngdədir?')#Her biri yeni setirde cap edilecek
+# print('Cavab: Ag')
+
+#Tapsiriq 1
+
+# print('Python')
+# print('   en populyar')
+# print('     dildir')
+
+
+# print('----------')
+# print('|         |')
+# print('|         |')
+# print('|         |')
+# print('----------')
+
+#Deyisenler
+
+# a = 5
+# a = 7
+# print(a)
+
+# a = 12 #case sensitive 
+# A = 21
+# _ = 25
+
+
+# print(a)
+
+##### Input funksiyası #####
+
+# a = input() # a = "Hello"
+# print("----------------")
+# print(a)
+
+#############
+# a = int(input("1-ci ədədi daxil edin: "))
+# b = int(input("2-ci ədədi daxil edin: "))
+# c = a + b # Salam + Hello = SalamHello
+# print("Cavab: ", c)
+
+import this
