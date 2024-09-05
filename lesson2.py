24f3284868d5cdd4769430b4fae79f6ba5dc4891
elcinbayramoff <bayramlielcin03@gmail.com>
2024-09-05 14:33:10 +0400

    Ders3 - Stringler

--- lesson2.py
+++ lesson2.py
@@ -56,12 +56,76 @@
 
 #3 eded cem, hasil ededi orta
 
-a = int(input("1ci ededi daxil edin: "))
-b = int(input("2ci ededi daxil edin: "))
-c = int(input("3cu ededi daxil edin: "))
+# a = int(input("1ci ededi daxil edin: "))
+# b = int(input("2ci ededi daxil edin: "))
+# c = int(input("3cu ededi daxil edin: "))
 
-cem = a + b + c
-hasil = a * b * c
-ededi_orta = cem / 3
+# cem = a + b + c
+# hasil = a * b * c
+# ededi_orta = cem / 3
 
-print("Cem ",cem, "Hasil ", hasil, "ededi orta ", ededi_orta)
+# print("Cem ",cem, "Hasil ", hasil, "ededi orta ", ededi_orta)
+
+
+# a = input("Şifrə daxil edin: ") #  a = "5"
+
+# a = int(input("Eded daxil edin: ")) 
+
+
+# print(a * 2)
+
+
+# Tapsiriq1
+# a1 = int(input("1ci ədədi daxil edin: "))
+# a2 = int(input("2ci ədədi daxil edin: "))
+# a3 = int(input("3cü ədədi daxil edin: "))
+
+# #cem
+
+# cem = a1 + a2 + a3
+
+# #Hasil
+
+# hasil = a1 * a2 * a3
+
+# #Ededi orta
+
+# ededi_orta = cem / 3
+
+# print(cem, hasil, ededi_orta)
+
+#Tapsiriq 2
+
+# x1 = float(input("X1:"))
+# x2 = float(input("X2:"))
+
+# y1 = float(input("Y1:"))
+# y2 = float(input("Y2:"))
+
+# kvadratlari_cemi = (x2-x1) ** 2 + (y2-y1) ** 2
+
+# import math
+
+# cavab = math.sqrt(kvadratlari_cemi)
+# print("Netice:", cavab)
+
+#Tapsiriq 3
+
+# a = int(input("Ededi daxil edin: "))
+
+# reqem1 = a % 10 
+
+# reqem2 = (a // 10) % 10
+
+# reqem3 = a // 100
+
+# print(reqem3, ",", reqem2, ",", reqem1 )
+# 1 , 2 , 3  
+#2345 // 100 = 23 %10 =3
+# 123  %  10 = 3   283 % 10 = 3    
+
+# 123 // 10 =  12  % 10 =  2 
+
+
+# 567 // 100 =  5    386 // 100 = 3 
+
---
+++ lesson3.py
@@ -0,0 +1,46 @@
+# a = "Python"
+
+# print(a[5])
+
+#String hisselere ayirmaq
+
+# a = "Azerbaijan"
+
+
+# print(a[7:3:-1]) #jiab
+
+#Stringlerin deyisilmesi
+
+# Python yazisinda t herfini c ile evez et
+
+# a = "Python" # Pychon  
+
+# deyisilmis_a = a[:2] + "c" + a[3:]
+
+# print(deyisilmis_a)
+
+#Stringlerin formatlanmasi
+
+# Tapsiriq1
+
+# a = 5
+# b = 4
+# #Senin yasin 15
+
+# txt = f'Salam{a}{b}'
+
+# print(txt)
+
+#Replace
+
+# a = 'Python'
+
+# modified_a = a.replace("t","c")
+
+# print(modified_a)
+
+#Elcin Bayramli 01/01/2000
+
+a = "Python"
+print(a[3:10])
+
